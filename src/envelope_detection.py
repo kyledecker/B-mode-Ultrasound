4fc6d195a21f1dab04d3280645f52894ff95281f
wjl11@duke.edu <wjl11@duke.edu>
2016-11-05 16:11:05 -0400

    added matrix input testing to envelope detection unit test, implemented and tested detect function (closes #11)

---
+++ src/envelope_detection.py
@@ -0,0 +1,22 @@
+import sys
+
+
+def detect(data, axis=-1):
+    """
+    perform envelope detection on input data by calculating the magnitude of
+    the analytic signal
+
+    :param data: input data to envelope detect
+    :param axis: dimension along which detection is performed, default: -1
+    :return: env_data (np.array)
+    """
+
+    from scipy.signal import hilbert
+    import numpy as np
+
+    data = np.array(data)
+
+    analytic_data = hilbert(data, N=None, axis=axis)
+    env_data = np.absolute(analytic_data)
+
+    return env_data
