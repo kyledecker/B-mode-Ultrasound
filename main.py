a1136748067147500aae41b456eb3efd30c288e3
wjl11@duke.edu <wjl11@duke.edu>
2016-11-05 16:21:18 -0400

    added detect in main

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 
 if __name__ == "__main__":
     from manipulate import reshape_rf
+    from envelope_detection import detect
 
+    # load in rf data from binary and scan parameters from JSON
+
+    # reshape the data based on scan geometry
     rf_image = reshape_rf(rf_vector, axial_samples, num_beams)
     """
     read in 1-d vector of rf data and shape into 2-d image based on number
@@ -12,5 +16,21 @@ if __name__ == "__main__":
     :param num_beams: number of transmit beams
     :return: matrix (np.array [num_beams][axial_samples])
     """
+
+    # perform envelope detection on rf image
+    env_image = detect(rf_image)
+    """
+    perform envelope detection on input data by calculating the magnitude of
+    the analytic signal
+
+    :param data: input data to envelope detect
+    :param axis: dimension along which detection is performed, default: -1
+    :return: env_data (np.array)
+    """
+
+    # log compress envelope detected image
+
+    # save/display final B-mode image
+
 
 
